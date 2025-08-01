e26e0a5a8572887d15913697f72b0298f5adcdb4
Guilherme Cavalini <Gui.marciellopes@outlook.com>
2025-07-31 21:58:11 -0300

    Alteração nome arquivo

--- src/components/Cronometro/BotoesModos/index.jsx
+++ src/components/Cronometro/BotoesModos/index.jsx
@@ -1,8 +1,6 @@
 import styles from "./styles.module.css";
 
 import BotaoModo from "./BotaoModo";
-import { useContext } from "react";
-import { CronometroContext } from "../../../context/CronometroContext";
 import useCronometroContext from "../../../hooks/useCronometroContext";
 
 const BotoesModos = () => {
